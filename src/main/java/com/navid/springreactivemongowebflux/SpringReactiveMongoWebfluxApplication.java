a5ab1b4f62ce4eeee65e9e1bef6a1459a6221af0
n.zare <n.zare@behsacorp.com>
2023-05-17 13:20:33 +0330

    first commit

---
+++ src/main/java/com/navid/springreactivemongowebflux/SpringReactiveMongoWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.navid.springreactivemongowebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveMongoWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveMongoWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/navid/springreactivemongowebflux/SpringReactiveMongoWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navid.springreactivemongowebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveMongoWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
